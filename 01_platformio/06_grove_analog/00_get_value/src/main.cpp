b8ba154715dc4c973bd568dc780bcb664d68ce3d
Satoru Osawa <osawa@takram.com>
2022-05-05 18:04:41 +0200

    Add get analog value

---
+++ 01_platformio/06_grove_analog/00_get_value/src/main.cpp
@@ -0,0 +1,25 @@
+#include <M5Stack.h>
+
+int address = 0x50;
+unsigned int value = 0;
+
+void setup() {
+  M5.begin();
+  Wire.begin();
+  M5.Lcd.setCursor(70, 0, 4);
+  M5.Lcd.println("ADC Sample");
+}
+
+void loop() {
+  Wire.requestFrom(address, 2);
+  if (Wire.available()) {
+    value = (Wire.read() & 0x0f) << 8;
+    value |= Wire.read();
+
+    M5.Lcd.setCursor(100, 50, 4);
+    M5.Lcd.print("Value:");
+    M5.Lcd.print(value);
+    M5.Lcd.print("      ");
+  }
+  delay(10);
+}
